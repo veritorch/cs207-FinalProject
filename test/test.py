3dd92da15a03588d19fd6b98234294074c698580
kevin5naug <kevinguan_37@outlook.com>
2019-12-04 14:48:14 -0500

    backward mode online

--- test/test.py
+++ test/test.py
@@ -42,6 +42,46 @@ def test_get_diff_continuous_usage():
     dx=sol.get_diff(g,[2,1])
     assert (dx==np.array([[12,0],[4,0]])).all()
 
+#test backward
+def testb_get_diff_scalar_to_scalar():
+    sol=vt.Solver(1)
+    def f(x):
+        return [x*x]
+    dx=sol.get_diff(f,[1],mode="backward")
+    assert (dx==np.array([2])).all()
+
+def testb_get_diff_vector_to_scalar():
+    sol=vt.Solver(2)
+    def f(x,y):
+        return [x*y]
+    dx=sol.get_diff(f,[1,2],mode="backward")
+    assert (dx==np.array([2,1])).all()
+
+def testb_get_diff_scalar_to_vector():
+    sol=vt.Solver(1)
+    def f(x):
+        return [x*x*x, 4*x]
+    dx=sol.get_diff(f,[2],mode="backward")
+    assert (dx==np.array([[12],[4]])).all()
+
+def testb_get_diff_vector_to_vector():
+    sol=vt.Solver(2)
+    def f(x,y):
+        return [x*y, x+y]
+    dx=sol.get_diff(f,[1,2],mode="backward")
+    assert (dx==np.array([[2,1],[1,1]])).all()
+
+def testb_get_diff_continuous_usage():
+    sol=vt.Solver(2)
+    def f(x,y):
+        return [x*y]
+    def g(x,y):
+        return [x**3, 4*x]
+    dx=sol.get_diff(f,[1,2],mode="backward")
+    assert (dx==np.array([2,1])).all()
+    dx=sol.get_diff(g,[2,1],mode="backward")
+    assert (dx==np.array([[12,0],[4,0]])).all()
+
 def test_neg():
     sol=vt.Solver(3)
     x1=sol.create_variable(4)
@@ -254,3 +294,208 @@ def test_arccos_out_of_range():
     with pytest.raises(ValueError):
         f=np.arccos(x1)
 
+
+#test backward mode variable
+def testb_neg():
+    x1=vt.Variable_b(4)
+    a = -x1
+    a.grad_value = 1.0
+    assert a.value == -4, "error with neg"
+    assert x1.grad() == -1, "error with neg"
+
+def testb_add():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    x3=vt.Variable_b(6)
+    f = x1+x2+x3
+    f.grad_value = 1.0
+    assert f.value == 15, "error with add"
+    assert x1.grad() == 1, "error with add"
+    assert x2.grad() == 1, "error with add"
+    assert x3.grad() == 1, "error with add"
+
+def testb_radd():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    x3=vt.Variable_b(6)
+    f = 4+x2+x3
+    f.grad_value = 1.0
+    assert f.value == 15, "error with radd"
+    assert x1.grad() == 0, "error with radd"
+    assert x2.grad() == 1, "error with radd"
+    assert x3.grad() == 1, "error with radd"
+
+def testb_sub():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    x3=vt.Variable_b(6)
+    f = x1-x2-3
+    f.grad_value = 1.0
+    assert f.value == -4, "error with sub"
+    assert x1.grad() == 1, "error with sub"
+    assert x2.grad() == -1, "error with sub"
+    assert x3.grad() == 0, "error with sub"
+
+def testb_rsub():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    x3=vt.Variable_b(6)
+    f = 11-x1-x3
+    f.grad_value = 1.0
+    assert f.value == 1, "error with rsub"
+    assert x1.grad() == -1, "error with rsub"
+    assert x2.grad() == 0, "error with rsub"
+    assert x3.grad() == -1, "error with rsub"
+
+def testb_mul():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    x3=vt.Variable_b(6)
+    f = x1*x2*x3*2
+    f.grad_value = 1.0
+    assert f.value == 240, "error with mul"
+    assert x1.grad() == 60, "error with mul"
+    assert x2.grad() == 48, "error with mul"
+    assert x3.grad() == 40, "error with mul"
+
+def testb_rmul():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    x3=vt.Variable_b(6)
+    f = 2*x1*x2*x3
+    f.grad_value = 1.0
+    assert f.value == 240, "error with rmul"
+    assert x1.grad() == 60, "error with rmul"
+    assert x2.grad() == 48, "error with rmul"
+    assert x3.grad() == 40, "error with rmul"
+
+def testb_truediv():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    f = x2/x1
+    f.grad_value = 1.0
+    assert f.value == 1.25, "error with truediv"
+    assert x1.grad() == -0.3125, "error with truediv"
+    assert x2.grad() == 0.25, "error with truediv"
+
+    x21=vt.Variable_b(4)
+    x22=vt.Variable_b(5)
+    f2 = x22/4
+    f2.grad_value = 1.0
+    assert f2.value == 1.25, "error with truediv"
+    assert x21.grad() == 0, "error with truediv"
+    assert x22.grad() == 0.25, "error with truediv"
+    
+    x31=vt.Variable_b(4)
+    x32=vt.Variable_b(5)
+    f3 = x32/(4*x31)
+    f3.grad_value = 1.0
+    assert f3.value == 5/16, "error with truediv"
+    assert x31.grad() == -0.078125, "error with truediv"
+    assert x32.grad() == 0.0625, "error with truediv"
+    
+    x41=vt.Variable_b(4)
+    x42=vt.Variable_b(5)
+    f4 = x42/(x41*x41)
+    f4.grad_value = 1.0
+    assert f4.value == 0.3125, "error with truediv"
+    assert x41.grad() == -0.15625, "error with truediv"
+    assert x42.grad() == 0.0625, "error with truediv"
+
+def testb_rtruediv():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    f = 5/x1
+    f.grad_value = 1.0
+    assert f.value == 1.25, "error with rtruediv"
+    assert x1.grad() == -0.3125, "error with rtruediv"
+    assert x2.grad() == 0, "error with rtruediv"
+
+    x21=vt.Variable_b(4)
+    x22=vt.Variable_b(5)
+    f2 = 4/(x21*x21)
+    f2.grad_value = 1.0
+    assert f2.value == 0.25, "error with rtruediv"
+    assert x21.grad() == -0.125, "error with rtruediv"
+    assert x22.grad() == 0, "error with rtruediv"
+
+def testb_pow():
+    x1=vt.Variable_b(4)
+    x2=vt.Variable_b(5)
+    f = (x1+x2) ** 2
+    f.grad_value = 1.0
+    assert f.value == 81, "error with pow"
+    assert x1.grad() == 18, "error with pow"
+    assert x2.grad() == 18, "error with pow"
+
+def testb_exp():
+    x1=vt.Variable_b(0)
+    x2=vt.Variable_b(5)
+    f = np.exp(x1) + x2
+    f.grad_value = 1.0
+    assert f.value == 6, "error with exp"
+    assert x1.grad() == 1, "error with exp"
+    assert x2.grad() == 1, "error with exp"
+
+def testb_log():
+    x1=vt.Variable_b(10)
+    x2=vt.Variable_b(5)
+    f = np.log(x1) + np.log(x2)
+    f.grad_value = 1.0
+    assert f.value == 3.9120230054281464, "error with exp"
+    assert x1.grad() == 0.1, "error with exp"
+    assert x2.grad() == 0.2, "error with exp"
+
+def testb_sin():
+    x1=vt.Variable_b(math.pi/2)
+    x2=vt.Variable_b(math.pi/6)
+    f = np.sin(x1) + np.sin(x2)
+    f.grad_value = 1.0
+    assert f.value == 1.5, "error with sin"
+    assert abs(x1.grad() -6.12323400e-17) < 10**(-8), "error with sin"
+    assert abs(x2.grad() -8.66025404e-01) < 10**(-8), "error with sin"
+
+def testb_cos():
+    x1=vt.Variable_b(math.pi/2)
+    x2=vt.Variable_b(math.pi/6)
+    f  = np.cos(x1) + np.cos(x2)
+    f.grad_value = 1.0
+    assert f.value == 0.8660254037844388, "error with cos"
+    assert abs(x1.grad() - (-1)) < 10**(-8), "error with cos"
+    assert abs(x2.grad() - (-0.5)) < 10**(-8), "error with cos"
+
+def testb_tan():
+    x1=vt.Variable_b(math.pi/2)
+    x2=vt.Variable_b(math.pi/6)
+    f = np.tan(x1) + np.tan(x2)
+    f.grad_value = 1.0
+    assert f.value == 1.633123935319537e+16, "error with tan"
+    assert abs(x1.grad() - (2.667093788113571e+32)) < 10**(-8), "error with tan"
+    assert abs(x2.grad() - (1.33333333e+00)) < 10**(-8), "error with tan"
+
+def testb_arcsin():
+    x1=vt.Variable_b(0.5)
+    x2=vt.Variable_b(0.1)
+    f = np.arcsin(x1) + np.arcsin(x2)
+    f.grad_value = 1.0
+    assert abs(f.value - 0.6237661967598587) < 1e-8, "error with arcsin"
+    assert abs(x1.grad() - (1.15470054)) < 10**(-8), "error with arcsin"
+    assert abs(x2.grad() - (1.00503782)) < 10**(-8), "error with arcsin"
+
+def testb_arccos():
+    x1=vt.Variable_b(0.5)
+    x2=vt.Variable_b(0.1)
+    f = np.arccos(x1) + np.arccos(x2)
+    f.grad_value = 1.0
+    assert abs(f.value - 2.5178264568299342) < 1e-8, "error with arccos"
+    assert abs(x1.grad() - (-1.15470054)) < 10**(-8), "error with arccos"
+    assert abs(x2.grad() - (-1.00503782)) < 10**(-8), "error with arccos"
+
+def testb_arctan():
+    x1=vt.Variable_b(0.5)
+    x2=vt.Variable_b(0.1)
+    f = np.arctan(x1) + np.arctan(x2)
+    f.grad_value = 1.0
+    assert abs(f.value - 0.5633162614919682) < 1e-8, "error with arctan"
+    assert abs(x1.grad() - (0.8)) < 10**(-8), "error with arctan"
+    assert abs(x2.grad() - (0.99009901)) < 10**(-8), "error with arctan"
--- veritorch/veritorch.py
+++ veritorch/veritorch.py
@@ -17,18 +17,26 @@ class Solver():
         self.n = n
         self.independent_variable_list = []
     
+    #create variable for forward mode
     def create_variable(self, x):
         der = np.zeros(self.n)
         der[len(self.independent_variable_list)] = 1
         var = Variable(x, der)
-        
         if len(self.independent_variable_list) < self.n: 
             self.independent_variable_list.append(var)
         else:
             raise Exception ("Trying to create more variables than specified") 
 
         return var
-
+    
+    #create variable for backward mode
+    def create_variable_b(self, x):
+        var_b = Variable_b(x)
+        if len(self.independent_variable_list) < self.n:
+            self.independent_variable_list.append(var_b)
+        else:
+            raise Exception ("Trying to create more variables than specified")
+        return var_b
     
     def get_variable(self, idx):
         stored_var = self.independent_variable_list[idx]
@@ -36,13 +44,15 @@ class Solver():
 
     def merge(self, d_list):
     	return np.stack(d_list, axis=0)
-
-    def get_diff(self, f, x):
+        
+    def get_diff_forward(self, f, x):
         #check arguments of f
+        #f must take in n independent variables
         sig = signature(f)
         if(len(sig.parameters)!=self.n):
         	raise TypeError("# of arguments of f != n")
         #check length of x
+        #user must provide n point values
         if(len(x)!=self.n):
             raise IndexError('len(x) != n')
         #reset solver
@@ -50,15 +60,69 @@ class Solver():
         for i in range(self.n):
             self.create_variable(x[i])
         ans=f(*self.independent_variable_list)
-        if(len(ans)>1):
+        if(len(ans)<=0):
+            raise TypeError("# of outputs of f <= 0")
+        elif(len(ans)>1):
             dans=[]
             for i in range(len(ans)):
                 dans.append(ans[i].dx)
+            #reset solver again
             self.independent_variable_list=[]
             return self.merge(dans)
         else:
+            #reset solver again
             self.independent_variable_list=[]
             return ans[0].dx
+    
+    def get_diff_backward(self, f, x):
+        #check arguments of f
+        #f must take in n independent variables
+        sig = signature(f)
+        if(len(sig.parameters)!=self.n):
+            raise TypeError("# of arguments of f != n")
+        #check length of x
+        #user must provide n point values
+        if(len(x)!=self.n):
+            raise IndexError('len(x) != n')
+        
+        #for backward mode, if the function has multiple outputs, due to our design issue, we must recreate all variable for each output of the function
+        #evaluate f to get number of outputs first
+        ans=f(*x)
+        if(len(ans)<=0):
+            raise TypeError("# of outputs of f <= 0")
+        elif(len(ans)==1):
+            self.independent_variable_list = []
+            for i in range(self.n):
+                self.create_variable_b(x[i])
+            ans=f(*self.independent_variable_list)
+            ans[0].grad_value = 1.0
+            dx=[]
+            for i in range(len(self.independent_variable_list)):
+                dx.append(self.independent_variable_list[i].grad())
+            dx=np.array(dx)
+            self.independent_variable_list=[]
+            return dx
+        else:
+            dans=[]
+            for i in range(len(ans)):
+                self.independent_variable_list=[]
+                for j in range(self.n):
+                    self.create_variable_b(x[j])
+                ans=f(*self.independent_variable_list)
+                ans[i].grad_value = 1.0
+                dx=[]
+                for j in range(len(self.independent_variable_list)):
+                    dx.append(self.independent_variable_list[j].grad())
+                dx=np.array(dx)
+                self.independent_variable_list=[]
+                dans.append(dx)
+            return self.merge(dans)
+            
+    def get_diff(self, f, x, mode="forward"):
+        if(mode=="forward"):
+            return self.get_diff_forward(f,x)
+        else:
+            return self.get_diff_backward(f,x)
 
     def __repr__(self):
         pass
@@ -643,3 +707,146 @@ class Variable():
         der = 1/(1+self.x**2) * self.dx
         return Variable(val, der)
 
+class Variable_b():
+    
+    def __init__(self, value):
+        """
+        Initiate Variable object.
+    
+        Parameters
+        =======
+        value: int/float; point of evaluation
+        
+        Returns
+        =======
+        None
+        """
+    
+        self.value = value
+        self.children = []
+        self.grad_value = None #None means it is not yet evaluated
+    
+    def __repr__(self):
+        #TODO
+        pass
+
+    def __str__(self):
+        #TODO
+        if self.grad_value == None:
+            return "Variable_b(" + str(self.value) + ", " + "None" + ")"
+        return "Variable_b(" + str(self.value) + ", " + str(self.grad_value) + ")"
+
+    def grad(self):
+        #TODO: DOCTEST
+        if self.grad_value is None: # recurse only if the value is not yet cached
+            self.grad_value = sum(weight * var.grad()
+                    for weight, var in self.children)#chain rule to calc derivative
+        return self.grad_value
+    
+    def __neg__(self):
+        z = Variable_b(-self.value)
+        self.children.append((-1, z))  # weight = ∂z/∂self = -1
+        return z
+    
+    def __add__(self, other):
+        try:
+            z = Variable_b(self.value + other.value)
+            other.children.append((1, z)) # weight = ∂z/∂self = 1
+            self.children.append((1, z)) # weight = ∂z/∂other = 1
+        except AttributeError:
+            z = Variable_b(self.value + other)
+            self.children.append((1, z)) # weight = ∂z/∂self = 1
+        return z
+    
+    def __radd__(self, other):
+        return self.__add__(other)
+    
+    def __sub__(self, other):
+        return self.__add__(-other)
+    
+    def __rsub__(self, other):
+        return -self.__sub__(other)
+    
+    def __mul__(self, other):
+        try:
+            z = Variable_b(self.value * other.value)
+            self.children.append((other.value, z)) # weight = ∂z/∂self = other.value
+            other.children.append((self.value, z)) # weight = ∂z/∂other = self.value
+        except AttributeError:
+            z = Variable_b(self.value * other)
+            self.children.append((other, z)) # weight = ∂z/∂self = other.value
+        return z
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+    
+    def __truediv__(self, other):
+        if isinstance(other, int):
+            if other == 0:
+                raise ValueError("Cannot divide by 0")
+        elif other.value ==0:
+            raise ValueError("Cannot divide by 0")
+        try:
+            z = Variable_b(self.value / other.value)
+            self.children.append(((1/other.value), z)) # weight = ∂z/∂self = 1/other.value
+            other.children.append((- self.value * (other.value) **(-2) , z)) # weight = ∂z/∂other = - self.value * (other.value) **(-2)
+        except AttributeError:
+            z = Variable_b(self.value / other)
+            self.children.append(((1/other), z)) # weight = ∂z/∂self = 1/other.value
+        return z
+    
+    def __rtruediv__(self, other):
+        if self.value == 0:
+            raise ValueError("Cannot divide by 0")
+        z = Variable_b(other/self.value)
+        self.children.append((-other * self.value ** (-2), z)) # weight = ∂z/∂elf = - other * (self.value) **(-2)
+        return z
+    
+    def __pow__(self, p):
+        z = Variable_b(self.value**p)
+        self.children.append((p*self.value**(p-1), z)) # weight = ∂z/∂self = p*self.value**(p-1)
+        return z
+    
+    def exp(self):
+        z = Variable_b(np.exp(self.value))
+        self.children.append((np.exp(self.value), z)) # weight = ∂z/∂self = exp(self.value)
+        return z
+    
+    def log(self):
+        z = Variable_b(np.log(self.value))
+        self.children.append((1/self.value, z)) # weight = ∂z/∂self = 1/self.value
+        return z
+    
+    def sin(self):
+        z = Variable_b(np.sin(self.value))
+        self.children.append((np.cos(self.value), z)) # weight = ∂z/∂self = np.cos(self.value)
+        return z
+    
+    def cos(self):
+        z = Variable_b(np.cos(self.value))
+        self.children.append((-np.sin(self.value), z)) # weight = ∂z/∂self = -np.sin(self.value)
+        return z
+    
+    def tan(self):
+        z = Variable_b(np.tan(self.value))
+        self.children.append((1/(np.cos(self.value))**2, z))
+        return z
+    
+    def arcsin(self):
+        if self.value >= 1 or self.value <= -1:
+            raise ValueError("arcsin does not exist beyond (-1,1)")
+        z = Variable_b(np.arcsin(self.value))
+        self.children.append((1/np.sqrt(1-self.value**2), z))
+        return z
+    
+    def arccos(self):
+        if self.value >= 1 or self.value <= -1:
+            raise ValueError("arccos does not exist beyond (-1,1)")
+        z = Variable_b(np.arccos(self.value))
+        self.children.append((-1/np.sqrt(1-self.value**2), z))
+        return z
+    
+    def arctan(self):
+        z = Variable_b(np.arctan(self.value))
+        self.children.append((1/(1+self.value**2), z))
+        return z
